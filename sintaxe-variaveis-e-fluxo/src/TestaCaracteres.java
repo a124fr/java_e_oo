300b6cf5fb8db117509287d4bb3d21babc7d3e30
Francisco de Assis <a124fr@gmail.com>
2022-02-28 11:59:26 -0300

    Trabalhando com tipo char

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,20 @@
+
+public class TestaCaracteres {
+	
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor = (char)(valor + 1);
+		System.out.println(valor);
+		
+		String texto = "Alura - Cursos online de tecnologia ";
+		System.out.println(texto);
+		
+		texto += 2020;
+		System.out.println(texto);
+	}
+}
