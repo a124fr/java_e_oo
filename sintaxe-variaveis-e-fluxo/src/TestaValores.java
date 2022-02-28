f2ccbc200dd6e1683466e36468d738dcb19f3991
Francisco de Assis <a124fr@gmail.com>
2022-02-28 12:06:51 -0300

    Trabalhando tipo valor

---
+++ sintaxe-variaveis-e-fluxo/src/TestaValores.java
@@ -0,0 +1,22 @@
+
+public class TestaValores {
+	
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		
+		// Tipo Valor - Valor do conteúdo da variável.
+		// Copiando e colando valores, atribuindo valores.
+		// Variáveis guardam valores e não referências
+		
+		System.out.println(segundo);
+		
+		segundo = primeiro;
+		
+		System.out.println(segundo);
+		
+		primeiro = 10;
+		
+		System.out.println(segundo);
+	}
+}
