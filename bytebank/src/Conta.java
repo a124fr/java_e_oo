388db9720bce3acca745ac65a4bd5f61eeb4e8fb
Francisco de Assis <a124fr@gmail.com>
2022-03-01 19:25:12 -0300

    trabalhando com comportamentos de classes, o comportamento que são os métodos

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -1,7 +1,27 @@
 
 public class Conta {
-	double saldo;	
-	int agencia = 42;
+	double saldo;
+	int agencia;
 	int numero;
-	String titular;	
+	String titular;
+
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {		
+		if (this.saldo > 0 && this.saldo >= valor) {
+			this.saldo -= valor;			
+			return true;
+		}		
+		return false;
+	}
+
+	public boolean transfere(double valor, Conta destino) {		
+		if (this.saca(valor)) {
+			destino.deposita(valor);
+			return true;
+		}				
+		return false;
+	}
 }
---
+++ bytebank/src/TestaMetodo.java
@@ -0,0 +1,27 @@
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+		boolean conseguiuRetirar = contaDoPaulo.saca(20.50);
+		System.out.println("O saque de R$20,50 foi feito? " + (conseguiuRetirar ? "Sim" : "Não"));
+		System.out.println(contaDoPaulo.saldo);
+
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+
+		boolean sucessoTransferencia = contaDaMarcela.transfere(300, contaDoPaulo);
+
+		if (sucessoTransferencia) {
+			System.out.println("transferência com sucesso");
+		} else {
+			System.out.println("faltou dinheiro");
+		}
+
+		System.out.println("Saldo da Conta do Marcela: R$" + contaDaMarcela.saldo);
+		System.out.println("Saldo da Conta do Paulo: R$" + contaDoPaulo.saldo);
+	}
+}
