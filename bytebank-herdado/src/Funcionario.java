1939a640dc8a3a58e5fbaffad30c2a0fe3666d6a
Francisco de Assis <a124fr@gmail.com>
2022-03-03 06:23:24 -0300

    Herança, sobrecarga de método

--- bytebank-herdado/src/Funcionario.java
+++ bytebank-herdado/src/Funcionario.java
@@ -1,11 +1,11 @@
-
+// classe mãe é chamada de super ou base class
 public class Funcionario {
 	private String nome;
 	private String cpf;
-	protected double salario;
+	private double salario;
 	
 	public double getBonificacao() {
-		return this.salario * 0.1;
+		return this.salario * 0.05;
 	}
 	
 	public String getNome() {
--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,4 +1,5 @@
 // Gerente é um Funcionario, Gerente herda da classe Funcionario
+// A classe filha também é chamada de sub class
 public class Gerente extends Funcionario {	
 	private int senha;
 	
@@ -13,13 +14,22 @@ public class Gerente extends Funcionario {
 		return false;
 	}
 	
-	// Reescrita de método
+	// Sobrecarga de método
+	// Na mesma classe que variam na quantidade ou tipos de parâmetros. 
+	// Isso se chama sobrecarga de métodos.
+	// A sobrecarga não leva em conta a visibilidade ou retorno do método, 
+	// apenas os parâmetros e não depende da herança.
+	public boolean autentica(String login, int senha) {
+		return false;
+	}
+	
+	// Reescrita de método ou sobrescrita
 	// A sobrescrita é um conceito importante na herança, pois permite 
 	// redefinir um comportamento previsto na classe mãe através da classe filha
 	// O método possui a mesma assinatura. Isto é, a mesma visibilidade, mesmo retorno, 
 	// mesmo nome e os mesmos parâmetros.
 	@Override
 	public double getBonificacao() {
-		return super.salario;
+		return super.getBonificacao() + super.getSalario();
 	}
 }
