fd85cefb78942ffd9a81ef7d1e5841f6f33eae4e
Francisco de Assis <a124fr@gmail.com>
2022-03-04 16:01:11 -0300

    Trabalhando com enumeração

---
+++ am_enum/src/Teste.java
@@ -0,0 +1,18 @@
+
+
+public class Teste {
+
+	public static void main(String[] args) {		
+		
+		Prioridade pMin = Prioridade.MIN;
+		Prioridade pMax = Prioridade.MAX;
+		
+		System.out.println(pMin.getValor());
+		System.out.println(pMin.name());
+		
+		System.out.println();
+		
+		System.out.println(pMax.getValor());
+		System.out.println(pMax.name());
+	}
+}
