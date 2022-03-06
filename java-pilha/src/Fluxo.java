b1a327e76fa7d36ae034953538d354681c39fb5e
Francisco de Assis <a124fr@gmail.com>
2022-03-05 23:18:23 -0300

    Trabalhando com exceções no código

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -2,20 +2,27 @@
 public class Fluxo {
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
-		metodo1();
+		try {
+			metodo1();
+		}
+		catch(ArithmeticException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 		System.out.println("Fim do main");
 	}
 
 	private static void metodo1() {
 		System.out.println("Ini do metodo1");
-		metodo2();
+		metodo2(); 
 		System.out.println("Fim do metodo1");
 	}
 
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
 		for (int i = 1; i <= 5; i++) {
-			System.out.println(i);
+			System.out.println(i);			
+			int a = i / 0;			
 		}
 		System.out.println("Fim do metodo2");
 	}
