be6857fa28d9e8cf04a984237c4bfd88b8bfe812
Francisco de Assis <a124fr@gmail.com>
2022-03-06 19:28:58 -0300

    Trabalhando com exceções checked ou unchecked

--- java-pilha/src/FluxoComTratamento2.java
+++ java-pilha/src/FluxoComTratamento2.java
@@ -12,13 +12,21 @@ public class FluxoComTratamento2 {
 		System.out.println("Fim do main");
 	}
 
-	private static void metodo1() {
+	private static void metodo1() throws MinhaExcecao {
 		System.out.println("Ini do metodo1");
 		metodo2(); 
 		System.out.println("Fim do metodo1");
 	}
+// Faz o tratamento com try/catch ou informar o throws na assinatura do método.	
+//	private static void metodo1() {
+//		System.out.println("Ini do metodo1");
+//		try {
+//			metodo2();
+//		} catch(MinhaExcecao ex) {}
+//		System.out.println("Fim do metodo1");
+//	}
 
-	private static void metodo2() {
+	private static void metodo2() throws MinhaExcecao {
 		System.out.println("Ini do metodo2");
 		
 		throw new MinhaExcecao("Deu ruim!");
