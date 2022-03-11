250fc85cf173f61883fb580d6364229a95307c1b
Francisco de Assis <a124fr@gmail.com>
2022-03-11 13:19:23 -0300

    Uma alternativa de como criar um array

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
@@ -3,10 +3,14 @@ package br.com.bytebank.banco.test;
 public class Teste {
 
 	public static void main(String[] args) {
-				
-		int[] idades2 = new int[] { 1, 2, 3, 4, 5};
+		
+		int idades4[] = new int[5];
+		idades4[0] = 8;
+		idades4[1] = 99;
 		
 		int[] idades3 = { 1, 22, 39, 4, 7, 9 };
+		
+		int[] idades2 = new int[] { 1, 2, 3, 4, 5};
 		
 		int[] idades = new int[5]; // o array é inicializado com 0
 		idades[0] = 29;
@@ -28,6 +32,11 @@ public class Teste {
 		System.out.println("Percorrendo array idades3");
 		for(int posicao = 0; posicao < idades3.length; posicao++) {
 			System.out.println(idades3[posicao]);
+		}
+		
+		System.out.println("Percorrendo array idades4");
+		for(int posicao = 0; posicao < idades4.length; posicao++) {
+			System.out.println(idades4[posicao]);
 		}
 	}
 }
