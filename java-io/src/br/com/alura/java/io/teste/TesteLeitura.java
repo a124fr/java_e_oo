18c975c2e22fbf106f549cf96b3c66a065ef4f50
Francisco de Assis <a124fr@gmail.com>
2022-03-25 02:14:06 -0300

    Leitura de arquivo com Classe FileInputStream, InputStreamReader e BufferedReader

---
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -0,0 +1,24 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TesteLeitura {
+
+	public static void main(String[] args) throws IOException {
+		
+		// Fluxo de Entrada com Arquivo
+		FileInputStream fis = new FileInputStream("lorem.txt"); // leitura de bits e bytes do fluxo do arquivo
+		InputStreamReader isr = new InputStreamReader(fis); // leitura de caractere do fluxo do arquivo
+		BufferedReader br = new BufferedReader(isr); // leitura da linha do arquivo.
+		
+		String linha = br.readLine();
+		System.out.println(linha);
+		
+		br.close();
+	}
+
+}
