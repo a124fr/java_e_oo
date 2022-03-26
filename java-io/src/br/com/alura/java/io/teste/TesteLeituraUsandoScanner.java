bf61f0f69f80eebfa781834b17574b3c613b822d
Francisco de Assis <a124fr@gmail.com>
2022-03-26 14:11:06 -0300

    Usando o Scanner para separar os dados

--- java-io/src/br/com/alura/java/io/teste/TesteLeituraUsandoScanner.java
+++ java-io/src/br/com/alura/java/io/teste/TesteLeituraUsandoScanner.java
@@ -2,6 +2,8 @@ package br.com.alura.java.io.teste;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeituraUsandoScanner {
@@ -12,7 +14,23 @@ public class TesteLeituraUsandoScanner {
 		while(entrada.hasNextLine()) {
 			String linha = entrada.nextLine();
 			System.out.println(linha);
-		}		
+			
+			Scanner linhaScanner = new Scanner(linha);			
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+			
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numeroDaConta = linhaScanner.nextInt(); 
+			String titular = linhaScanner.next();
+			Double saldo = linhaScanner.nextDouble(); 
+			
+			System.out.println(tipoConta 
+					+ ", Agência: " + agencia 
+					+ ", Número: " + numeroDaConta					
+					+ ", Titular: " + titular
+					+ ", Saldo: " + saldo);						
+		}					
 		entrada.close();
 	}
 }
